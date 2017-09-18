5eb57b177c1be12a94b4015c8fe2a1c662c73ce1
zzh1991 <zzh19910530@163.com>
2017-09-19 00:41:32 +0800

    table pagination and sort

--- src/main/script/main.js
+++ src/main/script/main.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+import { Table, Icon } from 'antd';
 
-const Main = ({children}) => {
-  return (
-    <div>
-      <div>{children}</div>
-    </div>
-  );
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  sorter: (a, b) => a.name.length - b.name.length,
+  width: 100,
+}, {
+    title: 'Age',
+    dataIndex: 'age',
+  sorter: (a, b) => a.age - b.age,
+  width: 100,
+}, {
+    title: 'Address',
+    dataIndex: 'address',
+  sorter: (a, b) => a.address.length - b.address.length,
+  width: 300,
+}];
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
+}
+
+
+
+class Main extends React.Component {
+  state = {
+    current: 1,
+  };
+
+  onChange = (pagination, filters, sorter) => {
+    console.log('params', pagination, sorter);
+  };
+
+  render () {
+    return (
+      <div style={{ width: '500px' }}>
+          <Table columns={columns} dataSource={data} pagination={{
+            pageSize: 12,
+            current: this.state.current,
+            defaultCurrent: 1,
+            total: data.length,
+            showSizeChanger: true,
+            onShowSizeChange: (current, pageSize) => {
+              console.log(current, pageSize);
+            },
+            onChange: (page) => {
+              console.log(page);
+              this.setState({
+                current: page,
+              });
+            },
+          }} onChange={this.onChange}
+          />
+      </div>
+    );
+  }
 };
 
-export default withRouter(Main);
+export default Main;
