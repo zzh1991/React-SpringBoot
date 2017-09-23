aadd5c923dfb8367132900bf3b5b0eee86d4b5dd
zhihao zhang <zzh19910530@163.com>
2017-09-23 09:00:41 +0800

    modify the sql

--- src/main/resources/db/migration/V201709202230__createStudentTable.sql
+++ src/main/resources/db/migration/V201709202230__createStudentTable.sql
@@ -1,7 +1,6 @@
+CREATE SEQUENCE IF NOT EXISTS public.student_id_seq START 1;
+
 CREATE TABLE IF NOT EXISTS public.student (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id BIGINT PRIMARY KEY DEFAULT nextval('public.student_id_seq'),
   name TEXT
 );
-
-
-
