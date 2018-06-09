440a20a81e0e14def6af698297aa83491ddd45b9
Zhihao Zhang <zzh19910530@163.com>
2018-06-09 20:40:42 +0800

    Update index.js

--- src/main/script/index.js
+++ src/main/script/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogRocket from 'logrocket';
 import Routes from './routes';
-
-LogRocket.init('zbiy5p/dev-test');
-LogRocket.identify('movie-helper', {
-  name: 'movie-helper',
-});
 
 ReactDOM.render(
   <Routes />,
