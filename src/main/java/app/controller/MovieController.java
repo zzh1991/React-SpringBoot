b2b44ceaba98a37ea29b44a941bbd076dfe46745
zzh1991 <zzh19910530@163.com>
2018-12-25 23:44:02 +0800

    remove unused code

--- src/main/java/app/controller/MovieController.java
+++ src/main/java/app/controller/MovieController.java
@@ -7,7 +7,6 @@ import app.vo.movie.MovieSubject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
