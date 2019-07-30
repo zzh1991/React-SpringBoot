08193c2552e9e303d8f2fe058d2a4e673822da91
zzh1991 <zzh19910530@163.com>
2019-07-30 23:32:56 +0800

    change travis config

--- src/main/java/app/mapper/FilmListMapper.java
+++ src/main/java/app/mapper/FilmListMapper.java
@@ -3,5 +3,10 @@ package app.mapper;
 import app.entity.FilmList;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+/**
+ * @author zhihao zhang
+ * @date 2019.07.30
+ */
+
 public interface FilmListMapper extends BaseMapper<FilmList> {
 }
