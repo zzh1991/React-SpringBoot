ffe6077fa09c6ce061e5ed5ae38b2b4ba5e44ca6
Zhihao Zhang <zzh19910530@163.com>
2020-12-06 11:30:16 +0800

    fix unit test error

--- src/test/java/app/controller/MovieApplicationTest.java
+++ src/test/java/app/controller/MovieApplicationTest.java
@@ -2,39 +2,30 @@ package app.controller;
 
 import app.entity.Film;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-import javax.annotation.PostConstruct;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static io.restassured.RestAssured.get;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MovieApplicationTest {
 
-    @LocalServerPort
-    private int port;
-    private String uri;
-
-    @PostConstruct
-    public void init() {
-        uri = "http://localhost:" + port;
-    }
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    public void getFilmListById() {
+    public void getFilmListById() throws Exception {
         Film film = Film.builder()
                 .id(1L)
                 .movieId(1L)
                 .build();
 
-        get(uri.concat("/list/").concat(film.getMovieId().toString()))
-        .then()
-        .assertThat()
-        .statusCode(HttpStatus.OK.value());
+        mvc.perform(get("/list/".concat(film.getMovieId().toString())))
+                .andExpect(status().isOk());
+
     }
 }
