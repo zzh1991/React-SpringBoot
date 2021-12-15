75ed962d99b1ae66c495b86c58d1f3a09ecdb935
Zhihao Zhang <zzh19910530@163.com>
2021-12-16 01:07:28 +0800

    swagger support spring boot 2.6.x

--- src/main/java/app/util/ConstantUtils.java
+++ src/main/java/app/util/ConstantUtils.java
@@ -1,14 +1,11 @@
 package app.util;
 
-import lombok.experimental.UtilityClass;
 
 /**
  * @author zhihao zhang
  * @since 3/8/20
  */
-
-@UtilityClass
-public class ConstantUtils {
-    public final String LARGE = "large";
-    public final String SEPARATOR = ",";
+public final class ConstantUtils {
+    public static final String LARGE = "large";
+    public static final String SEPARATOR = ",";
 }
